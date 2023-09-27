cb261b52d7e55e891226f0ca5d475635e8b0c09c
Jeysson Pino <jeyssonpino2@gmail.com>
2023-09-27 18:37:39 -0300

    Agregando gitignore

---
+++ clase1.py
@@ -0,0 +1,19 @@
+"""""#estudiantes = input("¿Cuantos estudiantes conectados hay?= ")
+
+#print("Hay " + estudiantes + " estudiantes conectados")
+
+#apellido = "Torres"
+#print(apellido[3])
+
+#numeros = "123456"
+
+#print(numeros[5])
+
+
+notas_del_examen1 = 5
+
+notas_del_examen2 = 9
+
+resultado = float(notas_del_examen1 + notas_del_examen2)/2
+
+print("El promedio de tus notas fue "+  (resultado))"""""
